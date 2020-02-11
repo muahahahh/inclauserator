30794259a394335ff6ee10418b29529f416cdff2
muahahahh <pavel061092@gmail.com>
2020-02-11 22:53:12 +0100

    added create statement

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, jsonify, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired
+import re
 
 class InputForm(FlaskForm):
     inclause_field_name = StringField('Database column name')
@@ -81,6 +82,60 @@ def casenate(pattern, excel, els):
 
 
 
+def create_table(table_name, excel):
+    string = "CREATE TABLE {} (\n".format(table_name)
+    dtypes = {1: 'INT',
+              3: 'DECIMAL',
+              5: 'DATE',
+              7: 'VARCHAR'}
+
+    excel = excel.split('\n')
+    excel = [x for x in excel if x]
+    n_columns = len(excel[0].split('\t'))
+    headers = {i: {'header': k, 'dtype': 0, 'len': [0, 0]} for i, k
+               in enumerate(excel[0].split("\t"))}
+    for i in range(1, n_columns):
+        for x, v in enumerate(excel[i].split("\t")):
+            typ = dtype(v)
+            if typ >= headers[x]['dtype']:
+                headers[x]['dtype'] = typ
+                if typ == 3:
+                    num = v.split(".")
+                    whole = len(num[0])
+                    frac = len(num[1])
+                    if whole > headers[x]['len'][0]:
+                        headers[x]['len'][0] = whole
+                    if frac > headers[i]['len'][1]:
+                        headers[x]['len'][1] = frac
+                if typ == 7:
+                    strlen = len(v)
+                    if strlen > headers[x]['len'][0]:
+                        headers[x]['len'][0] = strlen
+    for i in range(0, n_columns):
+        if typ == 3:
+            prop = '({},{})'.format(headers[i]['len'][0], headers[i]['len'][1])
+        elif typ == 7:
+            prop = '({})'.format(headers[i]['len'][0])
+        else:
+            prop = ''
+        col = '\t{} {}{},\n'.format(headers[i]['header'],
+                                    dtypes[headers[i]['dtype']],
+                                    prop)
+        string = string + col
+    string = string[0:-2] + ');'
+    return string
 
 
+def dtype(value):
+    integer = re.match('^\d+$', value)
+    decimal = re.match('^\d+\.\d+$', value)
+    date = re.match('^\d{4}(-)\d{1,2}(-)\d{1,2}$', value)
+    if integer is not None:
+        return 1
+    elif decimal is not None:
+        return 3
+    elif date is not None:
+        return 5
+    else:
+        return 7
 
